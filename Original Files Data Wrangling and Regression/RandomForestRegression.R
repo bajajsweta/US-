5faebb008ea1240b1989ca73f3489cb43d485931
Sweta <bajaj.sw@husky.neu.edu>
2017-03-15 21:44:41 -0400

    Reduced the Size of the Trees and the number of Variables

--- Original Files Data Wrangling and Regression/RandomForestRegression.R
+++ Original Files Data Wrangling and Regression/RandomForestRegression.R
@@ -27,11 +27,10 @@ train_ind_r <- sample(seq_len(nrow(QuarterData_RandomForestData)),size = smp_siz
 #Divide into Training and Testing Data
 train_r <- QuarterData_RandomForestData[train_ind_r,]
 test_r <- QuarterData_RandomForestData[-train_ind_r,]
-?randomForest.formula
 # Fitting model
 randomForestfit <- randomForest(ORIGINAL_INTEREST_RATE~CREDIT_SCORE+ORIGINAL_UPB+ORIGINAL_LOAN_TERM+MORTGAGE_INSURANCE_PERCENTAGE_MI+
-                                  ORIGINAL_COMBINED_LOAN_TO_VALUE_CLTV+ORIGINAL_DEBT_TO_INCOME_DTI_RATIO+PROPERTY_STATE+ORIGINAL_LOAN_TO_VALUE_LTV+
-                                  LOAN_PURPOSE+PROPERTY_TYPE+FIRST_TIME_HOMEBUYER_FLAG+NUMBER_OF_UNITS,data = train_r, ntree=50)
+                                  ORIGINAL_COMBINED_LOAN_TO_VALUE_CLTV+ORIGINAL_DEBT_TO_INCOME_DTI_RATIO+ORIGINAL_LOAN_TO_VALUE_LTV
+                                  ,data = train_r, ntree=5)
 summary(randomForestfit)
 #Predict Output 
 predictedrandomForest = predict(randomForestfit,test_r)
