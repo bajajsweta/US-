6276d3ae98bfd1d38b2c0b938379d58459fab7ae
antriksh22 <antriksh.a@husky.neu.edu>
2017-03-16 17:47:20 -0400

    Update LinearRegression.R

--- Original Files Data Wrangling and Regression/LinearRegression.R
+++ Original Files Data Wrangling and Regression/LinearRegression.R
@@ -1,5 +1,8 @@
-#Install Packages in R
+
+  #Install Packages in R
 install.packages("data.table")
+install.packages("forecast")
+install.packages("leaps")
 #install.packages("forecast")
 install.packages("leaps",repos='https://CRAN.R-project.org')
 install.packages("dplyr")
@@ -9,19 +12,24 @@ getwd()
 
 #Import the Libraries
 library(data.table)
+library(forecast)
 #library(forecast)
 library(leaps)
 library(dplyr)
 
+print("Loading Data...")
 print("Loading Data for Quarter2005")
 
 #Read the File from the Current Directory
-QuarterData <- fread("Quarter2005.csv", sep = ',')
+QuarterData <- fread("Sample_Original_Validated_ata1_Q42013.csv", sep = ',')
+
 QuarterData2 <- fread("Sample_Original_Validated_ata1_Q12014.csv.csv", sep = ',')
 print (head(QuarterData))
 print (head(QuarterData2))
 
+print("Loaded Data")
 
+print("Performing Variable Selection")
 #Writen to Accept the System Args from the Console
 args <- commandArgs(trailingOnly = TRUE)
 n <- as.numeric(args[1])
@@ -87,91 +95,142 @@ QuarterDataFeature2$LOAN_PURPOSE <- normalized(QuarterDataFeature2$LOAN_PURPOSE)
 QuarterDataFeature2$PROPERTY_TYPE <- normalized(QuarterDataFeature2$PROPERTY_TYPE)
 QuarterDataFeature2$FIRST_TIME_HOMEBUYER_FLAG <- normalized(QuarterDataFeature2$FIRST_TIME_HOMEBUYER_FLAG)
 
-# VARIABLE SELECTION
-# Four algorithms exhaustive search, forward, backward selection, stepwise regression are explored here
+ VARIABLE SELECTION
+ Four algorithms exhaustive search, forward, backward selection, stepwise regression are explored here
+print("Forward Search")
 
-# Forward Search
-# nvmax is the maximum size of subsets
+Forward Search
+nvmax is the maximum size of subsets
 regfit_forward = regsubsets(ORIGINAL_INTEREST_RATE~., data = QuarterDataFeature, really.big=TRUE, method="forward")
 regsummary_forward = summary(regfit_forward)
 
+print(regsummary_forward$adjr2)
+print(regsummary_forward$rss)
 regsummary_forward$adjr2
 regsummary_forward$rss
 
+print(coef(regfit_forward,8))
 coef(regfit_forward,8)
 
 plot(regfit_forward,scale="r2") 
 plot(regsummary_forward$rss, xlab='Number of variables',ylab='RSS', type='l',col="green") + title ("Forward")
-plot(regsummary_forward$adjr2, xlab='Number of variables',ylab='Adjr2', type='l',col="green") + title ("Forward")
-plot(regsummary_forward$bic, xlab='Number of variables',ylab='BIC', type='l',col="green") + title ("Forward")
-plot(regsummary_forward$cp, xlab='Number of variables',ylab='CP', type='l',col="green") + title ("Forward")
-
-# Backward Search
+#@@ -38,15 +79,14 @@ plot(regsummary_forward$adjr2, xlab='Number of variables',ylab='Adjr2', type='l'
+ plot(regsummary_forward$bic, xlab='Number of variables',ylab='BIC', type='l',col="green") + title ("Forward")
+ plot(regsummary_forward$cp, xlab='Number of variables',ylab='CP', type='l',col="green") + title ("Forward")
+                         
+                         print("Backward Search")
+                         # Backward Search
 regfit_backward = regsubsets(ORIGINAL_INTEREST_RATE~., data = QuarterDataFeature, really.big=TRUE, method="backward")
 regsummary_backward = summary(regfit_backward)
-
-regsummary_backward$adjr2
-regsummary_backward$rss
-
-coef(regfit_backward,8)
-
-plot(regfit_backward,scale="r2") 
-plot(regsummary_backward$rss, xlab='Number of variables',ylab='RSS', type='l',col="blue") + title ("Backward")
-plot(regsummary_backward$adjr2, xlab='Number of variables',ylab='Adjr2', type='l',col="blue") + title ("Backward")
-plot(regsummary_backward$bic, xlab='Number of variables',ylab='BIC', type='l',col="blue") + title ("Backward")
-plot(regsummary_backward$cp, xlab='Number of variables',ylab='CP', type='l',col="blue") + title ("Backward")
-
-# Exhaustive Search
-regfit_exhaustive = regsubsets(ORIGINAL_INTEREST_RATE~., data = QuarterDataFeature, really.big=TRUE)
-regsummary_exhaustive = summary(regfit_exhaustive)
-
-regsummary_exhaustive$adjr2
-regsummary_exhaustive$rss
-
-coef(regfit_exhaustive,8)
-
-plot(regfit_exhaustive,scale="r2") 
-plot(regsummary_exhaustive$rss, xlab='Number of variables',ylab='RSS', type='l',col="red") + title ("Exhaustive")
-plot(regsummary_exhaustive$adjr2, xlab='Number of variables',ylab='Adjr2', type='l',col="red") + title ("Exhaustive")
-plot(regsummary_exhaustive$bic, xlab='Number of variables',ylab='BIC', type='l',col="red") + title ("Exhaustive")
-plot(regsummary_exhaustive$cp, xlab='Number of variables',ylab='CP', type='l',col="red") + title ("Exhaustive")
-
-
-# MODEL THE DATA
-#75% of the Sample Size 
-smp_size <- floor(0.75 * nrow(QuarterDataFeature))
-
-set.seed(140)
-train_ind <- sample(seq_len(nrow(QuarterDataFeature)),size = smp_size)
-
- smp_size2 <- floor(nrow(QuarterDataFeature2))
+                         
+                  print(regsummary_backward$adjr2)
+                         print(regsummary_backward$rss)
+                         regsummary_backward$adjr2
+                         regsummary_backward$rss
+                         
+                         print(coef(regfit_backward,8))
+                         coef(regfit_backward,8)
+                         
+                        plot(regfit_backward,scale="r2") 
+                         plot(regsummary_backward$rss, xlab='Number of variables',ylab='RSS', type='l',col="blue") + title ("Backward")
+                #         @@ -54,17 +94,14 @@ plot(regsummary_backward$adjr2, xlab='Number of variables',ylab='Adjr2', type='l
+                                                  plot(regsummary_backward$bic, xlab='Number of variables',ylab='BIC', type='l',col="blue") + title ("Backward")
+                                                 plot(regsummary_backward$cp, xlab='Number of variables',ylab='CP', type='l',col="blue") + title ("Backward")
                                                   
- set.seed(140)
- train_ind2 <- sample(seq_len(nrow(QuarterDataFeature2)),size = smp_size)
                                                   
-
-#Divide into Training and Testing Data
-train <- QuarterDataFeature[train_ind,]
-test <- QuarterDataFeature[-train_ind,]
-
-train2 <- QuarterDataFeature2[train_ind2,]
-
-
-# Use the lm Function to Apply Linear Regression
-lm.fit = lm(ORIGINAL_INTEREST_RATE~CREDIT_SCORE+ORIGINAL_UPB+ORIGINAL_LOAN_TERM+MORTGAGE_INSURANCE_PERCENTAGE_MI+
-              ORIGINAL_COMBINED_LOAN_TO_VALUE_CLTV+ORIGINAL_DEBT_TO_INCOME_DTI_RATIO+PROPERTY_STATE+ORIGINAL_LOAN_TO_VALUE_LTV+
-              LOAN_PURPOSE+PROPERTY_TYPE+FIRST_TIME_HOMEBUYER_FLAG+NUMBER_OF_UNITS, 
-            data = train)
-summary(lm.fit)
-pred = predict(lm.fit, train2)
-accuracy(pred, train2$ORIGINAL_INTEREST_RATE)
-
-print(accuracy(pred, train2$ORIGINAL_INTEREST_RATE))
-
-
-
-
-
-
-
-
+                                       print("Exhaustive Search")
+                                                  
+                                                  # Exhaustive Search
+                                                 regfit_exhaustive = regsubsets(ORIGINAL_INTEREST_RATE~., data = QuarterDataFeature, really.big=TRUE)
+                                                  regsummary_exhaustive = summary(regfit_exhaustive)
+                                                  
+                                                  print(regsummary_exhaustive$adjr2)
+                                                  print(regsummary_exhaustive$rss)
+                                                  regsummary_exhaustive$adjr2
+                                                  regsummary_exhaustive$rss
+                                                  
+                                                 print(coef(regfit_exhaustive,8))
+                                                  coef(regfit_exhaustive,8)
+                                                  
+                                                  plot(regfit_exhaustive,scale="r2") 
+                                                  plot(regsummary_exhaustive$rss, xlab='Number of variables',ylab='RSS', type='l',col="red") + title ("Exhaustive")
+                                                  @@ -73,26 +110,6 @@ plot(regsummary_exhaustive$bic, xlab='Number of variables',ylab='BIC', type='l',
+                                                  plot(regsummary_exhaustive$cp, xlab='Number of variables',ylab='CP', type='l',col="red") + title ("Exhaustive")
+                                                  
+                                                  
+                                                  print("Feature Selection")
+                                                  #FEATURE ENGINEERING
+                                                  #create a separate dataframe
+                                                  QuarterDataFeature <- QuarterData
+                                                  QuarterDataFeature2 <- QuarterData2
+                                                  print("Convert to Factors")
+                                                  QuarterDataFeature <- QuarterDataFeature %>% mutate_if(is.character,as.factor)
+                                                  QuarterDataFeature2 <- QuarterDataFeature2 %>% mutate_if(is.character,as.factor)
+                                                  print("Remove certain Factors")
+                                                  QuarterDataFeature <- within(QuarterDataFeature, rm("SELLER_NAME","SUPER_CONFORMING_FLAG",
+                                                  "POSTAL_CODE","MSA","LOAN_SEQUENCE_NUMBER",
+                                                  "SERVICER_NAME","PRODUCT_TYPE","FIRST_PAYMENT_DATE",
+                                                  "MATURITY_DATE", "NUMBER_OF_BORROWERS"))
+                                                  QuarterDataFeature2 <- within(QuarterDataFeature2, rm("SELLER_NAME","SUPER_CONFORMING_FLAG",
+                                                                                                      "POSTAL_CODE","MSA","LOAN_SEQUENCE_NUMBER",
+                                                                                                      "SERVICER_NAME","PRODUCT_TYPE","FIRST_PAYMENT_DATE",
+                                                                                                      "MATURITY_DATE", "NUMBER_OF_BORROWERS"))
+                                                  
+                                                  
+                                                  print("Convert to Numeric for Regressing")
+                                                  #Convert All the Factors to Numerics
+                                                  QuarterDataFeature$PROPERTY_STATE <- as.numeric(QuarterDataFeature$PROPERTY_STATE)
+                                                  QuarterDataFeature$LOAN_PURPOSE <- as.numeric(QuarterDataFeature$LOAN_PURPOSE)
+                                                  QuarterDataFeature$PROPERTY_TYPE <- as.numeric(QuarterDataFeature$PROPERTY_TYPE)
+                                                  QuarterDataFeature$FIRST_TIME_HOMEBUYER_FLAG <- as.numeric(QuarterDataFeature$FIRST_TIME_HOMEBUYER_FLAG)
+                                                  
+                                                  QuarterDataFeature2$PROPERTY_STATE <- as.numeric(QuarterDataFeature2$PROPERTY_STATE)
+                                                  QuarterDataFeature2$LOAN_PURPOSE <- as.numeric(QuarterDataFeature2$LOAN_PURPOSE)
+                                                  QuarterDataFeature2$PROPERTY_TYPE <- as.numeric(QuarterDataFeature2$PROPERTY_TYPE)
+                                                  QuarterDataFeature2$FIRST_TIME_HOMEBUYER_FLAG <- as.numeric(QuarterDataFeature2$FIRST_TIME_HOMEBUYER_FLAG)
+                                                  
+                                                  
+                                                  # MODEL THE DATA
+                                                  #75% of the Sample Size 
+                                                  smp_size <- floor(0.75 * nrow(QuarterDataFeature))
+                                                  @@ -100,19 +117,25 @@ smp_size <- floor(0.75 * nrow(QuarterDataFeature))
+                                                  set.seed(140)
+                                                  train_ind <- sample(seq_len(nrow(QuarterDataFeature)),size = smp_size)
+                                                  
+                                                  smp_size2 <- floor(0.75 * nrow(QuarterDataFeature2))
+                                                  @@ -100,19 +117,25 @@ smp_size <- floor(0.75 * nrow(QuarterDataFeature2))
+                                                  set.seed(140)
+                                                  train_ind2 <- sample(seq_len(nrow(QuarterDataFeature2)),size = smp_size)
+                                                  
+                                                  
+                                                  
+                                                  print("Divide into Training and Testing Data")
+                                                  #Divide into Training and Testing Data
+                                                  train <- QuarterDataFeature[train_ind,]
+                                                  test <- QuarterDataFeature[-train_ind,]
+                                                  
+                                                  train2 <- QuarterDataFeature2[train_ind2,]
+                                                  test2 <- QuarterDataFeature2[-train_ind2,]
+                                                  
+                                                  print("Use the lm Function to Apply Linear Regression")
+                                                  # Use the lm Function to Apply Linear Regression
+                                                  lm.fit = lm(ORIGINAL_INTEREST_RATE~CREDIT_SCORE+ORIGINAL_UPB+ORIGINAL_LOAN_TERM+MORTGAGE_INSURANCE_PERCENTAGE_MI+
+                                                  ORIGINAL_COMBINED_LOAN_TO_VALUE_CLTV+ORIGINAL_DEBT_TO_INCOME_DTI_RATIO+PROPERTY_STATE+ORIGINAL_LOAN_TO_VALUE_LTV+
+                                                  LOAN_PURPOSE+PROPERTY_TYPE+FIRST_TIME_HOMEBUYER_FLAG+NUMBER_OF_UNITS, 
+                                                  ORIGINAL_COMBINED_LOAN_TO_VALUE_CLTV+ORIGINAL_DEBT_TO_INCOME_DTI_RATIO+PROPERTY_STATE+ORIGINAL_LOAN_TO_VALUE_LTV+
+                                                  LOAN_PURPOSE+PROPERTY_TYPE+FIRST_TIME_HOMEBUYER_FLAG+NUMBER_OF_UNITS, 
+                                                  data = train)
+                                                  
+                                                  summary(lm.fit)
+                                                  pred = predict(lm.fit, test2)
+                                                  accuracy(pred, train2$ORIGINAL_INTEREST_RATE)
+                                                  
+                                                  print(accuracy(pred, train2$ORIGINAL_INTEREST_RATE))
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
